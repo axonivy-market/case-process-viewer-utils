13b1e7ac518467523088542ec7a2348d3687a31a
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-09-14 09:34:04 +0200

    Migrate to 14.0.0-m33-SNAPSHOT

--- case-process-viewer-utils-core/src/com/axonivy/utils/caseprocessviewer/core/internal/ProcessViewerBuilder.java
+++ case-process-viewer-utils-core/src/com/axonivy/utils/caseprocessviewer/core/internal/ProcessViewerBuilder.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriBuilder;
 
 import org.apache.commons.lang3.StringUtils;
 
